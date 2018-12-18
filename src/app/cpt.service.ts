847ac5c2f2363f8ee1d81ae349c9b7c059cdb662
Unknown <clarika.91@gmail.com>
2018-12-18 09:28:23 +0100

    upload de fichier client vers serveur + ajout bouton pour envoyer un message au serveur

--- src/app/cpt.service.ts
+++ src/app/cpt.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
+import * as SocketIOFileUpload from 'socketio-file-upload';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,29 @@ export class CptService {
 
   constructor() {
     this.socket = io.connect('http://localhost:4000');
+
+    var uploader = new SocketIOFileUpload(this.socket);
+    uploader.listenOnInput(document.getElementById("siofu_input"));
+
+
     this.socket.on('cptUpdate', (cpt: number) => {
       console.log(cpt);
       this.Cpt = cpt;
     });
+    this.socket.on('message', (message: string) => {
+      console.log('msg du serveur : ' + message);
+    }); 
+   
+    /** var pseudo = prompt('Quel est votre pseudo ?');
+    this.socket.emit('petit_nouveau', pseudo);*/
+
+
+    var sock= this.socket;
+    document.getElementById('boutonVersServeur').addEventListener('click',function ()   {
+     console.log("Salut serveur, ça va ");
+       sock.emit('message', 'Salut serveur, ça va ?');
+    });
+
+
    }
 }
